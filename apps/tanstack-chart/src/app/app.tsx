656a9f46facca67052562ad5418d50a10c07d4ae
Kate <kate@gmail.com>
2022-07-16 21:30:42 -0400

    Add Tanstack app and install react chart

---
+++ apps/tanstack-chart/src/app/app.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+    </StyledApp>
+  );
+}
+
+export default App;
